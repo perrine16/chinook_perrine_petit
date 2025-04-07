ce75270ef4a68802ff8aca52773468338844213d
Perrine Petit <perrine.petit@student.isae-supaero.fr>
2025-04-07 14:15:14 +0200

    base de donnees

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -2,18 +2,24 @@ from django.db import models
 
 # Create your models here.
 class artist(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    def __str__(self):
+        return self.name
 
 class album(models.Model):
-    title = models.CharField(max_length=200)
     artist = models.ForeignKey(artist, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200,null=True,blank=True)
+    def __str__(self):
+        return self.artist
     
 class track(models.Model):
-    name = models.CharField(max_length=200)
-    composer = models.CharField(max_length=200)
+    album = models.ForeignKey(album, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    composer = models.CharField(max_length=200, null=True, blank = True)
     milliseconds = models.IntegerField(default=0)
     bytes = models.IntegerField(default=0)
     unitPrice = models.FloatField(default=0)
-    album = models.ForeignKey(album, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.album
 
     
